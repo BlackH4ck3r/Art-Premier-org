06e0e1fe0f75eb4e15b4c33dc2cfd60d7628e75c
BlackH4ck3r <ba-lcs-19-24@unima.ac.mw>
2025-10-24 03:30:39 +0200

    Enhance CORS configuration in user-check route
    Updated CORS headers to allow dynamic origin and credentials.

--- api/user-check/route.js
+++ api/user-check/route.js
@@ -6,7 +6,9 @@ const pool = new Pool({
 });
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin || '*';
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
